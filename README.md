6a6d62970680a4b5bce01ed6f35e42c3433e16e9
Vitaly Rodionov <vitaly.rodionov@cirrus.com>
2019-10-07 21:25:22 +0100

    Add comments

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# BITS converter.
+# BITS converter
+# Takes decimal, hex, octal or binary inputs 
+# and prints as a binary formated 32 bits output.
+#
--- bits.py
+++ bits.py
@@ -1,26 +1,36 @@
 #!/bin/python
 
-num  = input("val->  ")
-
-if num > 0xffffffff:
-    print("No more then 32 bits, please")
-    exit(-1)
+MAX = 0xffffffff
 
 output = ""
 formated = "  "
 
+num  = input("val->  ")
+
+# check for  MAX
+if num > MAX:
+    print("No more then 32 bits, please.")
+    exit(-1)
+
+# convert to binary form
 while num>0:
     output = str((num%2)) + output
     num = (num/2)
 
+# reverse
 output = output[::-1]
+
+# add missing bits
 while len(output)<32:
     output = output + "0"
+
+#reverse back
 output = output[::-1]
 
+# format
 for x in range(32):
     formated = formated + output[x] + "  "
-
+# print
 print(" ===============================================================================================")
 print(" 31 30 29 28 27 26 25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0")
 print( formated )
